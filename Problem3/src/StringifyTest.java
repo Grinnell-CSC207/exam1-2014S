79865c2e4fa6bcb2951983d6a2760e75f5185aff
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-12 09:42:42 -0600

    Added blank test for Stringify.

---
+++ Problem3/src/StringifyTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * A variety of tests of the stringify method.
+ * 
+ * @author Samuel A. Rebelsky
+ */
+public class StringifyTest
+{
+  // +-----------+---------------------------------------------------------
+  // | Utilities |
+  // +-----------+
+
+  // +-------+-------------------------------------------------------------
+  // | Tests |
+  // +-------+
+
+  /**
+   * A few simple tests.
+   */
+  @Test
+  public void test()
+  {
+   
+  } // test()
+
+} // class StringifyTest
