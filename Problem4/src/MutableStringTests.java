3870252a428ab544267af1b90949ed4d68d3afa9
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-12 09:44:21 -0600

    Added blank test for MutableString.

---
+++ Problem4/src/MutableStringTests.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * A variety of tests of the MutableString class.
+ * 
+ * @author Samuel A. Rebelsky
+ */
+public class MutableStringTests
+{
+  // +-----------+---------------------------------------------------------
+  // | Utilities |
+  // +-----------+
+
+  // +-------+-------------------------------------------------------------
+  // | Tests |
+  // +-------+
+
+  /**
+   * A few simple tests.
+   */
+  @Test
+  public void test()
+  {
+   
+  } // test()
+
+} // class MutableStringTests
