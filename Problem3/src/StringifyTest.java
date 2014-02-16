084d38393bf4205774538482f36002a7c3099109
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-16 16:10:45 -0600

    Add simple unit test for stringify.

--- Problem3/src/StringifyTest.java
+++ Problem3/src/StringifyTest.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 
 /**
  * A variety of tests of the stringify method.
@@ -10,9 +13,57 @@ import org.junit.Test;
  */
 public class StringifyTest
 {
+  // +-----------+-------------------------------------------------------
+  // | Constants |
+  // +-----------+
+
+  /**
+   * The name of the file used for storing temporary output.
+   */
+  static final String TEMPFILE = "/tmp/stringifytest.txt"; 
+
   // +-----------+---------------------------------------------------------
   // | Utilities |
   // +-----------+
+
+  /**
+   * Read just the first line of a file.  Prints the exception and
+   * returns the empty string if I/O fails.  (Printing exceptions is
+   * generally not a good idea.  We do it here because it seems useful
+   * for testing.)
+   */
+  static String firstLine(String fname)
+  {
+    try
+      {
+        BufferedReader feyes = new BufferedReader(new FileReader(fname));
+        String result = feyes.readLine();
+        feyes.close();
+        return result;
+      } // try
+    catch (Exception e)
+      {
+        System.err.println(e);
+        return "";
+      } // catch (Exception)
+  } // firstLine
+
+  /**
+   * Check whether stringify produces output identical to that 
+   * stored in expected when called on infile.  
+   */
+  static void checkStringify(String infile, String expected)
+      throws Exception
+  {
+    TextUtils.stringify(infile, TEMPFILE);
+    String actual = firstLine(TEMPFILE);
+    String expect = firstLine(expected);
+    if (! actual.equals(expect)) 
+      {
+        fail("In converting '" + infile + "' got '" + actual +
+             "' and expected '" + expect + "'");
+      } // if (! actual.equals(expect))
+  } // checkStringify
 
   // +-------+-------------------------------------------------------------
   // | Tests |
@@ -23,8 +74,12 @@ public class StringifyTest
    */
   @Test
   public void test()
+      throws Exception
   {
-   
+    checkStringify("examples/in01.txt", "examples/out01.txt");
+    checkStringify("examples/in02.txt", "examples/out02.txt");
+    checkStringify("examples/in03.txt", "examples/out03.txt");
+    checkStringify("examples/in04.txt", "examples/out04.txt");
   } // test()
 
 } // class StringifyTest
